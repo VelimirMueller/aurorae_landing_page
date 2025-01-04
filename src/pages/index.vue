090288751dc3ba459d5b1cd4dee447effb6a589c
velimir <velimir.mueller@googlemail.com>
2025-01-04 17:05:25 +0100

    add placeholder pages so routing via vue-router can be tested

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,12 +2,31 @@
   <section class="hero">
     <LandingHero />
   </section>
+
+  <section class="logo-line">
+    <LogoLine />
+  </section>
+
+  <section class="cards pa-md-12">
+    <LandingCards />
+  </section>
 </template>
 
 <style scoped>
-.hero {
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-  }
+.hero, .cards {
+  background-color: #fff;
+}
+
+.text-dark {
+  color: rgb(var(--v-theme-textDark));
+  width: 100%;
+}
+
+.text-highlight {
+  color: rgb(var(--v-theme-secondary));
+}
+
+.logo-line {
+  background-color: rgb(var(--v-theme-primary));
+}
 </style>
