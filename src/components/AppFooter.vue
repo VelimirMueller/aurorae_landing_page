81d27dc406d8cfc6235999c1262fab9e7bc06a08
velimir <velimir.mueller@googlemail.com>
2025-01-04 17:01:58 +0100

    Add app footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer class="bg-primary h-100">
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        href="link"
+        class="mx-2 link"
+        rounded="xl"
+        variant="text">
+        {{ link.replace('/', '') }}
+      </v-btn>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+const links = [
+  '/about',
+  '/products',
+  '/pricing',
+  '/contact'
+]
+</script>
+
