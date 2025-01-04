08eae74e337f3a9717ff59d5a791bc9766b6cdc4
velimir <velimir.mueller@googlemail.com>
2025-01-04 17:04:27 +0100

    add first vuetify theme to landing page - so the colors can be changed conveniently as a theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -15,5 +15,22 @@ import { createVuetify } from 'vuetify'
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
-  },
+    themes: {
+      dark: {
+        dark: true, // Set to true for a dark theme
+        colors: {
+          primary: '#1A012C',
+          secondary: '#087A8E',
+          'secondary-light': '10bebe',
+          accent: '#FF39D4',
+          error: '#ED4E4E',
+          info: '#2ED6E2',
+          success: '#22CA68',
+          warning: '#FFCA58',
+          textDark: '#3c3c3c',
+          textLight: '#D9D9D9'
+        },
+      },
+    }
+  }
 })
