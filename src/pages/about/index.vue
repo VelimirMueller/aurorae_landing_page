f501402d0763e64b104ab2de089c421a34f78ffb
velimir <velimir.mueller@googlemail.com>
2025-01-11 15:15:00 +0100

    add about page with content

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>test</h1>
+  <section class="bg-white min-h-100 d-flex align-itmes">
+    <v-row>
+      <v-col 
+        cols="12" 
+        md="5"
+        l="4"
+        xl="4"
+        class="pa-12 d-lg-flex d-none align-center">
+        <img 
+          class="w-100"
+          src="../../assets/about_hero_image.webp" 
+          alt="">
+      </v-col>
+
+      <v-col class="d-flex align-center">
+        <article class="text-h6 pa-8 pa-md-12">
+          <h1 class="text-h2 text-sm-h1 text-center pt-12 font-weight-bold">
+            About Aurorae
+          </h1>
+
+          <p class="w-100 mt-8">
+            At <span class="text-secondary">Aurorae</span>, our mission is to revolutionize web applications by seamlessly integrating
+            <span class="text-secondary">cutting-edge AI tooling into</span> your digital experience. Powered by <span class="text-secondary">GPT4All</span>, 
+            our solutions bring unparalleled intelligence to your websites and apps, enabling businesses 
+            to unlock their full potential.
+          </p>
+
+          <p class="w-100 mt-4">
+            From <span class="text-secondary">AI chatbots</span> to <span class="text-secondary">image classification</span> and <span class="text-secondary">model training</span>,
+            <span class="text-secondary">Aurorae</span> empowers you with intuitive, customizable, and independent tools that are designed to elevate productivity and streamline your workflows. 
+            Whether you're enhancing customer interactions or managing complex datasets, 
+            our <span class="text-secondary">AI-driven platforms</span> ensure you stay ahead in a competitive world.
+          </p>
+
+          <p class="w-100 mt-4">
+            Explore the <span class="text-secondary">future of AI in web applications with Aurorae</span> and transform your vision into an intelligent reality.
+          </p>
+        </article>
+      </v-col>
+    </v-row>
+  </section>
 </template>
+
+<style scoped>
+.min-h-100 {
+  min-height: 90vh;
+}
+</style>
