3fdf7ab3978d45aa8368561049267778aaa2da64
velimir <velimir.mueller@googlemail.com>
2025-01-16 17:14:53 +0100

    add form validation composable - helps also to handle forms throughout the application

---
+++ src/composables/useForms.ts
@@ -0,0 +1,61 @@
+export interface VuetifyFormElement extends HTMLFormElement {
+  validate: () => Promise<{ valid: boolean }>
+  reset: () => never
+}
+
+export const validateFormSubmit = async (form: Ref<VuetifyFormElement|undefined>, callback: () => any): Promise<void> => {
+  if (!form.value) return alert('Form is not available')
+  
+  try {
+    const {valid} = await form.value.validate()
+    console.log(valid);
+    
+    if (!valid) { 
+      if (confirm('Form is invalid, reset form?')) {
+        resetForm(form)
+      }
+     }
+    else {
+      callback()
+    }
+  } catch (err) {
+    alert('Could not validate form due to error: ' + err)
+  }
+}
+
+export const resetForm = (form: Ref<VuetifyFormElement|undefined>): void => {
+  if (!form.value) return alert('Form is not available')
+
+  try {
+    form.value.reset()
+  } catch (err) {
+    console.error('Could not reset form due to error: ', err)
+  }
+}
+
+export const submit = (isLoading: Ref<boolean>, payload: Ref<object>, endpoint: string, redirectRoute: string): void => {
+  isLoading.value = true
+  try {
+    fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload.value)
+  })
+    .then(async (response) => {
+      if (response?.ok) {
+        window.location.href = redirectRoute
+      } else {
+        const res = await response.json()
+        alert(res.message)
+        console.error(`Error with response from /api/v1/register POST: status: ${response.status} statusText: ${response.statusText}`)
+      }
+    })
+    .finally(() => {
+      isLoading.value = false
+    })
+  } catch (err) {
+    console.error('error:', err)
+  }
+}
