e28e2119e6e4a0a04945f99d20f56a65f9a38492
velimir <velimir.mueller@googlemail.com>
2025-01-17 23:37:34 +0100

    code complexity: fix sonarcloud issue (https://sonarcloud.io/project/issues?open=AZR0hb3XphNAP1KvRN1W&id=VelimirMueller_aurorae_landing_page)

--- src/composables/useObserveIntersection.ts
+++ src/composables/useObserveIntersection.ts
@@ -1,25 +1,32 @@
 export const useObserveIntersectionsAnimation = (htmlElementSelector: string, firstAnimationClass: string, secondAnimationClass: string) => {
   onMounted(() => {
     const sections = document.querySelectorAll(htmlElementSelector) as unknown as HTMLElement[]; // Select the LogoLine section
+    
     sections.forEach((section, idx) => {
       if (idx !== 0) { 
         section.style.opacity = '0%';
-      }
+      } 
       
-      if (section) {
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting && idx !== 0) {
-              section.style.opacity = '100%'
-              idx % 2 === 0 ? section.classList.add(firstAnimationClass) : section.classList.add(secondAnimationClass)
-            }
-          })
-        }, 
-        { 
-          threshold: 0.25 // Ensures full visibility
-        })
-        observer.observe(section);
-      }
+      const observer = observerFactory(section, idx, firstAnimationClass, secondAnimationClass)
+      observer.observe(section)
     })
   })
+}
+
+const observerFactory = (section: HTMLElement, index: number, firstAnimationClass: string, secondAnimationClass: string) => {
+  return new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (index !== 0) { setObserverAnimation(section, index, firstAnimationClass, secondAnimationClass) }
+      }
+    })
+  }, 
+  { 
+    threshold: 0.25 // Ensures full visibility
+  })
+}
+
+const setObserverAnimation = (sectionElement: HTMLElement, sectionIndex: number, firstAnimationClass: string, secondAnimationClass: string): void => {
+  sectionElement.style.opacity = '100%'
+  sectionIndex % 2 === 0 ? sectionElement.classList.add(firstAnimationClass) : sectionElement.classList.add(secondAnimationClass)
 }
