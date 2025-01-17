ab3a619357d117f769f2f8b5db2119693eca8e12
velimir <velimir.mueller@googlemail.com>
2025-01-17 14:03:28 +0100

    improve visuals for landing page on tablet views

--- src/components/pages/landing/LandingFeature.vue
+++ src/components/pages/landing/LandingFeature.vue
@@ -4,13 +4,13 @@
   <v-row :class="`pa-md-12 ${reverseCols ? 'flex-row-reverse' : ''}`">
     <v-col 
       cols="12" 
-      md="6">
+      md="7">
       <slot name="left" />
     </v-col>
 
     <v-col 
       cols="12" 
-      md="6">
+      md="5">
       <slot name="right" />
     </v-col>
   </v-row>
