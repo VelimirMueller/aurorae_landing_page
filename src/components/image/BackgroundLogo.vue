21a79a3ff6894b4a5e951e57bc5a59ac385050ce
VelimirMueller <velimir.mueller@googlemail.com>
2025-01-21 21:34:53 +0100

    refactor: adjust image size by adding a wrapper
    the idea was to prevent some warning about adjusting the screen size directly

--- src/components/image/BackgroundLogo.vue
+++ src/components/image/BackgroundLogo.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="position-absolute h-75 d-none d-md-flex ml-auto hidden">
-    <img 
-      class="opacity-10"
-      src="@/assets/about_hero_image.webp" 
-      alt="Aurorae background Logo">
+    <div class="wh-250">
+      <img
+        alt="Aurorae background Logo"
+        class="opacity-10 w-100 h-100"
+        src="@/assets/about_hero_image.webp">
+    </div>
   </div>
 </template>
+<script lang="ts" setup>
+</script>
 <style>
 .hidden {
   overflow-y: hidden;
   left: 0;
+}
+
+.wh-250 {
+  width: 350px;
+  height: 350px;
 }
 </style>
