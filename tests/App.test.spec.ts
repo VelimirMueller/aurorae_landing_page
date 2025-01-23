6e1b35c6e95317a8713d63d142fb7e770813d77d
VelimirMueller <velimir.mueller@googlemail.com>
2025-01-23 12:40:56 +0100

    tests: add first set of component tests by making sure every page can be mounted

--- tests/App.test.spec.ts
+++ tests/App.test.spec.ts
@@ -1,28 +1,34 @@
-import { expect, test } from 'vitest'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { describe, expect, test } from 'vitest'
+import { DefineComponent } from 'vue'
 import ResizeObserver from 'resize-observer-polyfill'
-
+import { vuetifyShallowWrapper } from './helpers/shallowVuetiyWrapper'
 import LandingPage from '../src/pages/index.vue'
-import { shallowMount } from '@vue/test-utils'
+import AboutPage from '../src/pages/about/index.vue'
+import AdminPage from '../src/pages/admin/index.vue'
+import ProductsPage from '../src/pages/products/index.vue'
+import Signin from '../src/pages/signin/index.vue'
+import WikiPage from '../src/pages/wiki/index.vue'
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
+type Page = { name: string, component: DefineComponent }
+const pages: Page[] = [
+  { name: 'LandingPage', component: LandingPage as unknown as DefineComponent },
+  { name: 'AboutPage', component: AboutPage as unknown as DefineComponent },
+  { name: 'AdminPage', component: AdminPage as unknown as DefineComponent },
+  { name: 'ProductsPage', component: ProductsPage as unknown as DefineComponent },
+  { name: 'Signin', component: Signin as unknown as DefineComponent },
+  { name: 'WikiPage', component: WikiPage as unknown as DefineComponent }
+]
 
 global.ResizeObserver = ResizeObserver
 
 describe('Basic app functionality testing', () => {
-  test('Can mount landing page', () => {
-    const wrapper = shallowMount(LandingPage, {
-      props: {},
-      global: {
-        plugins: [vuetify]
-      }
-    })
+  let wrapper: ReturnType<typeof vuetifyShallowWrapper>
 
-    expect(wrapper.html()).toBeDefined()
+  pages.forEach((page) => {
+    test(`Can mount ${page.name}`, () => {
+      wrapper = vuetifyShallowWrapper(page.component)
+      expect(wrapper.exists()).toBe(true)
+      wrapper.unmount()
+    })
   })
 })
