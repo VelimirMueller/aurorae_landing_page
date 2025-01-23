b44abe073568f4e4ff93ccd24b51627ff2783519
VelimirMueller <velimir.mueller@googlemail.com>
2025-01-23 12:51:45 +0100

    tests: add first set of component tests by making sure every page can be mounted with succeeding tsts

--- tests/helpers/shallowVuetiyWrapper.ts
+++ tests/helpers/shallowVuetiyWrapper.ts
@@ -3,23 +3,21 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { DefineComponent } from 'vue'
-import { createPinia, setActivePinia } from 'pinia'
+import { createPinia, Pinia, setActivePinia } from 'pinia'
 
 const vuetify = createVuetify({
   components,
   directives
 })
 
-const pinia = createPinia()
+const pinia: Pinia = createPinia()
 setActivePinia(pinia)
 
-export const vuetifyShallowWrapper = (component: DefineComponent, plugins: any[] = [], ...attrs: any): ReturnType<typeof shallowMount> => {
+export const vuetifyShallowWrapper = (component: DefineComponent, ...attrs: any): ReturnType<typeof shallowMount> => {
   return shallowMount(component, {
     global: {
       plugins: [
-        vuetify,
-        setActivePinia(pinia),
-        ...plugins
+        vuetify
       ]
     },
     ...attrs
